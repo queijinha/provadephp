8a01dc81ebc6cc71c52e24f69b0a395038acf7af
geannn1612 <geanlucas13572500@gmail.com>
2024-11-29 16:30:48 -0300

    prova php

---
+++ index.php
@@ -0,0 +1,39 @@
+<h2>Receita:
+    <?php
+    echo "Bolo de cenoura"
+    ?>
+    </h2>
+    <table border = ="1">
+        <thead>
+            <tr>
+                <th>Ingredientes</th>
+                <th>Quantidade</th>
+</tr>
+</thead>
+<tbody>
+    <tr>
+        <td>Cenouras média raladas</td>
+        <td>3</td>
+    </tr>
+    <tr>
+        <td>Ovos</td>
+        <td>4</td>
+    </tr>
+    <tr>
+        <td>Farinha de trigo</td>
+        <td>2 e 1/2 xícaras (chá)</td>
+    </tr>
+    <tr>
+        <td>Óleo</td>
+        <td>1/2 xícara (chá)</td>
+    </tr>
+    <tr>
+        <td>Açúcar</td>
+        <td>2 xícaras (chá)</td>
+    </tr>
+    <tr>
+        <td>Fermento em pó</td>
+        <td>1 colher (sopa)</td>
+    </tr>
+</tbody>
+</table>
